112fc39108b1310843738cbaa7b7bf9255192eba
Bjarki Eldon <beldon11@gmail.com>
2022-10-25 18:05:52 +0200

    header file for Lsitesalllinked.cpp
    Header file for Lsitesalllinked.cpp containing globablly used constants

---
+++ Lsitesalllinked.hpp
@@ -0,0 +1,19 @@
+const unsigned long GLOBAL_CONST_N = 50000 ;
+const unsigned long GLOBAL_CONST_CARRYING_CAPACITY = 2*GLOBAL_CONST_N ;
+const unsigned long GLOBAL_CONST_CUTOFF = 2*GLOBAL_CONST_N;
+const double GLOBAL_CONST_CUTOFFd = static_cast<double>(GLOBAL_CONST_CUTOFF) ;
+const double GLOBAL_CONST_ALPHA_ONE = 0.75 ;
+const double GLOBAL_CONST_ALPHA_TWO = 3. ;
+const double GLOBAL_CONST_EPSILON = 0.1 ;
+const double GLOBAL_CONST_RECOMBINATION = 0.25 ; 
+const int GLOBAL_CONST_NUMBER_SITES = 2 ;
+const double GLOBAL_CONST_SELECTION = 0.1 ;
+const double GLOBAL_CONST_PROBABILITY_BOTTLENECK = -0.1 ;
+const unsigned long GLOBAL_CONST_BOTTLENECK = 1000 ;
+const double GLOBAL_CONST_NUMBER_SITESd = static_cast<double>(GLOBAL_CONST_NUMBER_SITES) ;
+/* make sure the length of  string "11...1"  and number of sites match */
+const std::string GLOBAL_CONST_ONES (GLOBAL_CONST_NUMBER_SITES, '1') ;
+const unsigned long GLOBAL_CONST_MAX_INDEX =  std::bitset<GLOBAL_CONST_NUMBER_SITES>(GLOBAL_CONST_ONES).to_ulong() ;
+/* total number of possible phased L-site types */
+const unsigned long GLOBAL_CONST_TOTAL_NUMBER_PHASED_TYPES = (GLOBAL_CONST_MAX_INDEX + 1)*(GLOBAL_CONST_MAX_INDEX + 2) / 2 ; 
+const int GLOBAL_CONST_NUMBER_EXPERIMENTS = 10 ;
